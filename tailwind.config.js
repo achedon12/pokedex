a0eaaa10e38a01b7debe397df254832d1f6bb9f7
leo <leo@confluent-digital.com>
2025-02-04 17:28:45 +0100

    website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: 'selector',
+    theme: {
+        extend: {
+            colors: {
+                'primary': 'var(--color-primary)',
+                'primary-hover': 'var(--color-primary-hover)',
+                'secondary': 'var(--color-secondary)',
+                'tertiary': 'var(--color-tertiary)',
+                'section-color': 'var(--color-section)',
+                'text-color': 'var(--text-color)',
+            },
+        },
+    },
+    plugins: [],
+}
