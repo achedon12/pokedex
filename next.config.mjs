3193e712052bcee57c410c7b776121723d43d143
leo <leo@confluent-digital.com>
2025-02-04 14:55:39 +0100

    :rocket: feat deploy.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/app',
+    images: {
+        unoptimized: true,
+    }
+};
 
 export default nextConfig;
